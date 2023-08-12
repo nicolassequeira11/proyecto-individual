15217ba9f69e224071db6db051be622b0f43db59
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2023-08-12 01:29:10 -0300

    Registro

--- js/login.js
+++ js/login.js
@@ -1,5 +1,18 @@
 const btnLogin = document.getElementById("btnLogin");
+const email = document.getElementById("emailLogin");
+const pass = document.getElementById("passLogin");
 
-btnLogin.addEventListener("click", ()=> {
-    window.location.href = "index.html";
+btnLogin.addEventListener("click", ()=>{
+    if(email.checkValidity() && pass.checkValidity()){
+        window.location.href = "index.html";
+    }
 });
+
+if (window.sessionStorage) {
+    sessionStorage.setItem("nombre", "Gonzalo");
+  
+    var nombre = sessionStorage.getItem("nombre");
+  
+    sessionStorage.removeItem("nombre");
+  
+  }
